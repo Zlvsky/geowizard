2bd1392cc32c45b3cf1745e5af046701d918ca90
Zlvsky <zlvsky@icloud.com>
2024-12-04 16:46:34 +0100

    fix: blank page on github pages

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/layout/header/Header'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import PageWrap from './components/layout/containers/PageWrap'
 import { TutorialPopup } from './components/tutorial-popup/TutorialPopup'
 import { Toaster } from './components/ui/sonner'
